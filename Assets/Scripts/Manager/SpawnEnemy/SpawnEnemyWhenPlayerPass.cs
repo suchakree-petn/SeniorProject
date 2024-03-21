0cb68250524f835bdf6ecc41c6add746ca66e4fb
Nor_Book <thanawat.srit@kmutt.ac.th>
2024-03-21 11:03:22 +0700

    UPDATE: Spawn Enemy collider trigger

--- Assets/Scripts/Manager/SpawnEnemy/SpawnEnemyWhenPlayerPass.cs
+++ Assets/Scripts/Manager/SpawnEnemy/SpawnEnemyWhenPlayerPass.cs
@@ -11,44 +11,26 @@ public class SpawnEnemyWhenPlayerPass : NetworkBehaviour
         if (!IsServer || !IsOwner) return;
         if (other.transform.root.TryGetComponent<PlayerController>(out _)){
             gameObject.GetComponent<Collider>().enabled = false;
-            Spawn_ServerRpc();
+            Spawn();
         }
         
     }
     private void Update() {
         if(Input.GetKeyDown(KeyCode.Alpha1)){
             foreach(GameObject enemy in enemys){
-                EnemyManager.Instance.Spawn(2001,enemy.transform.position);
+                ulong id = ulong.Parse(enemy.name.Split("_")[0]);
+                EnemyManager.Instance.Spawn(id,enemy.transform.position);
             }
         }
     }
-    [ServerRpc(RequireOwnership = false)]
-    public void Spawn_ServerRpc(){
+    // [ServerRpc(RequireOwnership = false)]
+    public void Spawn(){
         
         if(enemys != null)
             foreach(GameObject enemy in enemys){
-                enemy.SetActive(true);
-                enemy.GetComponent<NetworkObject>().Spawn(true);
-                // StartCoroutine(DelayActiveObj(enemy,0.5f));
+                ulong id = ulong.Parse(enemy.name.Split("_")[0]);
+                EnemyManager.Instance.Spawn(id,enemy.transform.position);
             }
-    }
-    // public override void OnNetworkSpawn()
-    // {
-    //     if (!IsServer) return;
-    //     if(enemys != null)
-    //     foreach(GameObject enemy in enemys){
-    //         enemy.SetActive(false);
-    //         enemy.GetComponent<NetworkObject>().Despawn();
-    //     }
-    // }
-    // private void Start() {
-        
-    // }
-    IEnumerator DelayActiveObj(GameObject enemy,float time){
-        yield return new WaitForSeconds(time);
-        enemy.SetActive(true);
-        enemy.GetComponent<NetworkObject>().Spawn();
-        
     }
     
 }
